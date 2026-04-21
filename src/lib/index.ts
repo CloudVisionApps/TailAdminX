efd8c87a5d07ae70c08d5cd7a34138da2e2c41a0
Bozhidar <bobicloudvision@gmail.com>
2026-04-21 14:20:20 +0300

    up

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,95 @@
-export { default as Button } from "../components/ui/button/Button";
-export { default as Badge } from "../components/ui/badge/Badge";
-export { default as Avatar } from "../components/ui/avatar/Avatar";
+import "./styles.css";
 
+export { default as SignInForm } from "../components/auth/SignInForm";
+export { default as SignUpForm } from "../components/auth/SignUpForm";
+
+export { default as BarChartOne } from "../components/charts/bar/BarChartOne";
+export { default as LineChartOne } from "../components/charts/line/LineChartOne";
+
+export { default as ChartTab } from "../components/common/ChartTab";
+export { default as ComponentCard } from "../components/common/ComponentCard";
+export { default as GridShape } from "../components/common/GridShape";
+export { default as PageBreadcrumb } from "../components/common/PageBreadCrumb";
+export { default as PageMeta } from "../components/common/PageMeta";
+export { default as RadiusSelector } from "../components/common/RadiusSelector";
+export { ScrollToTop } from "../components/common/ScrollToTop";
+export { ThemeToggleButton } from "../components/common/ThemeToggleButton";
+export { default as ThemeTogglerTwo } from "../components/common/ThemeTogglerTwo";
+export { AppWrapper } from "../components/common/PageMeta";
+
+export { default as CountryMap } from "../components/ecommerce/CountryMap";
+export { default as DemographicCard } from "../components/ecommerce/DemographicCard";
+export { default as EcommerceMetrics } from "../components/ecommerce/EcommerceMetrics";
+export { default as MonthlySalesChart } from "../components/ecommerce/MonthlySalesChart";
+export { default as MonthlyTarget } from "../components/ecommerce/MonthlyTarget";
+export { default as RecentOrders } from "../components/ecommerce/RecentOrders";
+export { default as StatisticsChart } from "../components/ecommerce/StatisticsChart";
+
+export { default as Form } from "../components/form/Form";
 export { default as Label } from "../components/form/Label";
-export { default as InputField } from "../components/form/input/InputField";
+export { default as MultiSelect } from "../components/form/MultiSelect";
 export { default as Select } from "../components/form/Select";
+export { default as DatePicker } from "../components/form/date-picker";
+export { default as DefaultInputs } from "../components/form/form-elements/DefaultInputs";
+export { default as DropZone } from "../components/form/form-elements/DropZone";
+export { default as FileInputExample } from "../components/form/form-elements/FileInputExample";
+export { default as InputGroup } from "../components/form/form-elements/InputGroup";
+export { default as InputStates } from "../components/form/form-elements/InputStates";
+export { default as RadioButtons } from "../components/form/form-elements/RadioButtons";
+export { default as SelectInputs } from "../components/form/form-elements/SelectInputs";
+export { default as TextAreaInput } from "../components/form/form-elements/TextAreaInput";
+export { default as ToggleSwitch } from "../components/form/form-elements/ToggleSwitch";
+export { default as PhoneInput } from "../components/form/group-input/PhoneInput";
+export { default as Checkbox } from "../components/form/input/Checkbox";
+export { default as FileInput } from "../components/form/input/FileInput";
+export { default as InputField } from "../components/form/input/InputField";
+export { default as Radio } from "../components/form/input/Radio";
+export { default as RadioSm } from "../components/form/input/RadioSm";
+export { default as TextArea } from "../components/form/input/TextArea";
+export { default as Switch } from "../components/form/switch/Switch";
+
+export { default as Header } from "../components/header/Header";
+export { default as NotificationDropdown } from "../components/header/NotificationDropdown";
+export { default as UserDropdown } from "../components/header/UserDropdown";
+
+export { default as BasicTableOne } from "../components/tables/BasicTables/BasicTableOne";
+
+export { default as UserAddressCard } from "../components/UserProfile/UserAddressCard";
+export { default as UserInfoCard } from "../components/UserProfile/UserInfoCard";
+export { default as UserMetaCard } from "../components/UserProfile/UserMetaCard";
+
+export { default as AppLayout } from "../layout/AppLayout";
+export { default as AppHeader } from "../layout/AppHeader";
+export { default as AppSidebar } from "../layout/AppSidebar";
+export { default as Backdrop } from "../layout/Backdrop";
+export { default as SidebarWidget } from "../layout/SidebarWidget";
+export { default as AuthLayout } from "../pages/AuthPages/AuthPageLayout";
+
+export { default as Alert } from "../components/ui/alert/Alert";
+export { default as Avatar } from "../components/ui/avatar/Avatar";
+export { default as Badge } from "../components/ui/badge/Badge";
+export { default as Button } from "../components/ui/button/Button";
+export { Dropdown } from "../components/ui/dropdown/Dropdown";
+export { DropdownItem } from "../components/ui/dropdown/DropdownItem";
+export { default as ResponsiveImage } from "../components/ui/images/ResponsiveImage";
+export { default as ThreeColumnImageGrid } from "../components/ui/images/ThreeColumnImageGrid";
+export { default as TwoColumnImageGrid } from "../components/ui/images/TwoColumnImageGrid";
+export { Modal } from "../components/ui/modal";
+export {
+  Table,
+  TableHeader,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "../components/ui/table";
+export { default as AspectRatioVideo } from "../components/ui/videos/AspectRatioVideo";
+export { default as FourIsToThree } from "../components/ui/videos/FourIsToThree";
+export { default as OneIsToOne } from "../components/ui/videos/OneIsToOne";
+export { default as SixteenIsToNine } from "../components/ui/videos/SixteenIsToNine";
+export { default as TwentyOneIsToNine } from "../components/ui/videos/TwentyOneIsToNine";
+
+export { SidebarProvider, useSidebar } from "../context/SidebarContext";
+export { ThemeProvider, useTheme, type Radius } from "../context/ThemeContext";
+
+export { default as useGoBack } from "../hooks/useGoBack";
+export { useModal } from "../hooks/useModal";
--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+        exportType: "named",
+        namedExport: "ReactComponent",
+      },
+    }),
+  ],
   build: {
     lib: {
       entry: "src/lib/index.ts",
